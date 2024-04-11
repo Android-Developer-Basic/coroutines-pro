5c557cd4ba2c4d7efc9f4fd1eb5d1ccf8df9fca4
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-04-11 16:38:36 +0200

    Basic error try/catch

---
+++ src/main/kotlin/ru/otus/errors/errors.kt
@@ -0,0 +1,36 @@
+package ru.otus.errors
+
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.async
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import kotlin.system.measureTimeMillis
+
+fun main() {
+    runBlocking {
+        launch {
+            try {
+                withContext(Dispatchers.IO) {
+                    badFun()
+                }
+            } catch (e: Exception) {
+                println("Caught $e")
+            }
+        }
+    }
+}
+
+suspend fun badFun() {
+    delay(100L)
+    throw RuntimeException("Error")
+}
