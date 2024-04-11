218059a42484bed682f1c1ae93604d2084e8c128
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-04-11 15:08:46 +0200

    Another context - not bound

--- src/main/kotlin/ru/otus/homework/contexts.kt
+++ src/main/kotlin/ru/otus/homework/contexts.kt
@@ -1,11 +1,13 @@
 package ru.otus.homework
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    launch {
+    CoroutineScope(Dispatchers.Default).launch {
         delay(500L)
         println("world!")
     }
