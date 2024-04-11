2904432e055baf0930e6cc407119af8fdc1d7c98
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-04-11 15:33:09 +0200

    Job fails if child fails

--- src/main/kotlin/ru/otus/homework/contexts.kt
+++ src/main/kotlin/ru/otus/homework/contexts.kt
@@ -1,16 +1,25 @@
 package ru.otus.homework
 
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
 fun main() = runBlocking {
-    val job = CoroutineScope(Dispatchers.Default).launch {
-        delay(500L)
-        println("world!")
+    val job = Job()
+    val scope = CoroutineScope(Dispatchers.Default + job)
+    scope.launch {
+        delay(300L)
+        throw Exception("launch1: error!")
     }
-    println("Hello")
+    scope.launch {
+        delay(1000L)
+        println("launch2: done!")
+    }
+    println("blocking waits for job to finish!")
     job.join()
 }
